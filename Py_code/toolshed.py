2847a487721c8199af8d360f589cac054962906a
Darian Hole <darian.hole@canada.ca>
2023-11-27 16:01:15 -0600

    adjust singularity command to see if that works

--- Py_code/toolshed.py
+++ Py_code/toolshed.py
@@ -526,7 +526,8 @@ def create_singularity_cmd(base_path, run_dir, image, cmd):
     Build singularity command
     '''
     # Create command using the bind path, image_location and cmd
-    full_cmd = f'singularity exec --bind {base_path} {image} cd {run_dir} && {cmd}'
+    #  May need to re-eval the '' quotes for the command as some commands were wrapped with ""
+    full_cmd = f"singularity exec --bind {base_path} {image} /bin/bash -c 'cd {run_dir} ; {cmd}'"
     return full_cmd
 
 ###############################################################################
